010b7bf8607e1dd713602bf3137299b28b276be2
amanmishra75way <amishra.75way@gmail.com>
2025-06-23 13:12:22 +0530

    trying animation 1

--- frontend/src/pages/Motion.jsx
+++ frontend/src/pages/Motion.jsx
@@ -3,7 +3,19 @@ import { motion } from "motion/react";
 const Motion = () => {
   return (
     <div>
-      <motion.div className="h-80 w-80 bg-red-500 border bored-white rounded-md"></motion.div>
+      <motion.div
+        className="h-80 w-80 bg-red-500 border bored-white rounded-md"
+        animate={{
+          rotate: 360,
+          scale: 1.2,
+          transition: {
+            duration: 2,
+            ease: "easeInOut",
+            repeat: Infinity,
+            repeatType: "reverse",
+          },
+        }}
+      ></motion.div>
     </div>
   );
 };
